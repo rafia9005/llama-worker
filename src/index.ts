a6dc1873ff9bab22dc928dfed6f407c270b5b5b9
rafia9005 <rafia9005@gmail.com>
2025-02-05 19:22:53 +0700

    wip(management session): init session on worker

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,54 @@
+interface Message {
+	role: 'user' | 'system' | 'assistant';
+	content: string;
+}
+
+interface AI {
+	run(model: string, params: { messages: Message[] }): Promise<string>;
+}
+
 export interface Env {
-  AI: Ai;
+	AI: AI;
+}
+
+interface RequestBody {
+	message: string;
 }
 
 export default {
-  async fetch(request, env): Promise<Response> {
+	async fetch(request: Request, env: Env): Promise<Response> {
+		let store: Message[] = [];
 
-    const messages = [
-      { role: "system", content: "You are a friendly assistant" },
-      {
-        role: "user",
-        content: "What is the origin of the phrase Hello, World",
-      },
-    ];
-    const response = await env.AI.run("@hf/thebloke/llama-2-13b-chat-awq", { messages });
+		try {
+			if (request.method === 'POST') {
+				const input = (await request.json()) as RequestBody;
+				const userMessage: Message = { role: 'user', content: input.message };
+				store.push(userMessage);
 
-    return Response.json(response);
-  },
+				const systemMessage: Message = { role: 'system', content: 'You are a friendly assistant' };
+				store.push(systemMessage);
+
+				const response = await env.AI.run('@hf/thebloke/llama-2-13b-chat-awq', {
+					messages: store,
+				});
+
+				const aiMessage: Message = { role: 'assistant', content: response };
+				store.push(aiMessage);
+
+				return Response.json({ response, store });
+			}
+
+			if (request.method === 'GET') {
+				return Response.json({ store });
+			}
+
+			return new Response('Method not allowed', { status: 405 });
+		} catch (error: unknown) {
+			if (error instanceof Error) {
+				//return new Response(`Error: ${error.message}`, { status: 500 });
+				throw error;
+			}
+			return new Response('Unknown error occurred', { status: 500 });
+		}
+	},
 } satisfies ExportedHandler<Env>;
